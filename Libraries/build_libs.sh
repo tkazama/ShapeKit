22b9791a9c1f8b6531c9a7a842ac5917a7214a69
Andrea Cremaschi <andreacremaschi@libero.it>
2013-02-18 09:06:40 -0800

    update iOS SDK to 6.1 cosmetic fixes to compile folder structure

--- Libraries/build_libs.sh
+++ Libraries/build_libs.sh
@@ -4,6 +4,7 @@ mkdir -p src/
 mkdir -p libs/
 mkdir -p libs/bin
 
+
 echo "Downlaoding GEOS source..."
 cd src/
 curl -O "http://download.osgeo.org/geos/geos-3.3.5.tar.bz2"
@@ -16,12 +17,12 @@ cp ../build_ios geos-3.3.5
 
 echo "Building GEOS..."
 cd geos-3.3.5
-sh build_ios -p $PWD/device_build device
+sh build_ios -p $PWD/build/device_build device
 make clean
-sh build_ios -p $PWD/simulator_build simulator
-lipo -arch i386 simulator_build/lib/libgeos.a -arch armv7 device_build/lib/libgeos.a -create -output ../../libs/bin/libgeos.a
-lipo -arch i386 simulator_build/lib/libgeos_c.a -arch armv7 device_build/lib/libgeos_c.a -create -output ../../libs/bin/libgeos_c.a
-cp -R simulator_build/include ../../libs/
+sh build_ios -p $PWD/build/simulator_build simulator
+lipo -arch i386 $PWD/build/simulator_build/lib/libgeos.a -arch armv7 $PWD/build/device_build/lib/libgeos.a -create -output ../../libs/bin/libgeos.a
+lipo -arch i386 $PWD/build/simulator_build/lib/libgeos_c.a -arch armv7 $PWD/build/device_build/lib/libgeos_c.a -create -output ../../libs/bin/libgeos_c.a
+cp -R build/simulator_build/include ../../libs/
 cd ..
 
 echo "Downloading PROJ4 source..."
@@ -35,11 +36,11 @@ cp ../build_ios proj-4.8.0
 
 echo "building PROJ4"
 cd proj-4.8.0
-sh build_ios -p $PWD/device_build device
+sh build_ios -p $PWD/build/device_build device
 make clean
-sh build_ios -p $PWD/simulator_build simulator
-lipo -arch i386 simulator_build/lib/libproj.a -arch armv7 device_build/lib/libproj.a -create -output ../../libs/bin/libproj.a
-cp -R simulator_build/include/* ../../libs/include
+sh build_ios -p $PWD/build/simulator_build simulator
+lipo -arch i386 $PWD/build/simulator_build/lib/libproj.a -arch armv7 $PWD/build/device_build/lib/libproj.a -create -output ../../libs/bin/libproj.a
+cp -R build/simulator_build/include/* ../../libs/include
 cd ../..
 
 echo "All Done!"
